ba9bfa5524c4bfb4d70581bd1bf61b8d656dcff9
Zonyra Hubert <123190677+zonyra-hubert@users.noreply.github.com>
2024-12-10 14:45:02 +0000

    Update Book.java

--- app/src/test/java/org/example/Book.java
+++ app/src/test/java/org/example/Book.java
@@ -1,26 +1,28 @@
 package org.example;
 
 public class Book {
-    private String title;         // The title of the book
-    private String author;        // The author of the book
-    private int yearPublished;    // The year the book was published
+    private String title;         // Title of the book
+    private String author;        // Author of the book
+    private int yearPublished;    // Year the book was published
 
-    // Constructor: Initializes the book details
+    // Constructor to initialize the attributes
     public Book(String title, String author, int yearPublished) {
         this.title = title;
         this.author = author;
         this.yearPublished = yearPublished;
     }
 
-    // Getters to access the private fields
+    // Getter for title
     public String getTitle() {
         return title;
     }
 
+    // Getter for author
     public String getAuthor() {
         return author;
     }
 
+    // Getter for yearPublished
     public int getYearPublished() {
         return yearPublished;
     }
