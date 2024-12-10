8a69e1629c026caf849550e335d8522be0acfe6e
Zonyra Hubert <123190677+zonyra-hubert@users.noreply.github.com>
2024-12-10 14:38:00 +0000

    Add files via upload

---
+++ app/src/test/java/org/example/Book.java
@@ -0,0 +1,27 @@
+package LMS;
+
+public class Book {
+    private String title;         // The title of the book
+    private String author;        // The author of the book
+    private int yearPublished;    // The year the book was published
+
+    // Constructor: Initializes the book details
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    // Getters to access the private fields
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+}
